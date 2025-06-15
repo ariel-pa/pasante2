fea291f97e9cfa5e0e710519f58e8b20bb25f957
ariel-pa <arielpachecoalejandro@gmail.com>
2025-06-14 22:19:54 -0400

    feat: new endpoint

--- src/routes/test.js
+++ src/routes/test.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const {getTest} = require("./../controllers/test.js");
+const { getTest } = require("./../controllers/test.js");
 
 /**
  * path to test
  */
 router.get("/", getTest);
+
+/**
+ * path to new endpoint
+ */
+router.get("/new-endpoint", (req, res) => {
+    res.status(200).json({
+        message: "new endpoint",
+    });
+});
 
 module.exports = router;
